8a0125b9d31358624271fc09de913f66d880a060
Ingryd <ingrydcduarte@gmail.com>
2025-03-05 11:48:34 -0300

    setup: test files

---
+++ TronchettoTests/TronchettoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TronchettoTests.swift
+//  TronchettoTests
+//
+//  Created by Ingryd Cordeiro Duarte on 05/03/25.
+//
+
+import Testing
+@testable import Tronchetto
+
+struct TronchettoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
